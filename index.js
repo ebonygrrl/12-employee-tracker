0273b165544869d8e6c73436db8af1ca7ef932d9
ebonygrrl <abooker77@yahoo.com>
2022-12-28 05:15:52 -0600

    attempting to delay init cb

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+// database connection
+const myDb = require('./utils/connection');
+
 // get classes
 const dbQuery = require('./lib/query-db');
 const modifyDb = require('./lib/modify-db');
@@ -7,6 +10,7 @@ const inquirer = require('inquirer');
 
 // inquirer choices
 const initOptions = ['View All Departments', 'View All Roles', 'View All Employees', 'Add a Department', 'Add a Role', 'Add An Employee', 'Update An Employee Role'];
+const selectDepts = ['Engineering', 'Finance', 'Legal', 'Sales'];
 const selectRoles = ['Sales Lead', 'Salesperson', 'Lead Engineer', 'Software Engineer', 'Account Manager', 'Accountant', 'Legal Team Lead', 'Lawyer'];
 const selectEmployee = [];
 
@@ -40,7 +44,7 @@ const addRoleQuestions = [
         type: 'list',
         name: 'role_dept',
         message: 'Which department does the role belong to?',
-        choices: selectRoles,
+        choices: selectDepts,
     }
 ];
 
@@ -98,62 +102,80 @@ const updateRoleQuestions = [
 
 
 const init = () => {
-    let connect;
+    inquirer.prompt([{
+        type: 'list',
+        name: 'start',
+        message: 'What would you like to do?',
+        choices: initOptions,
+    }]).then(answer => {   
 
-    inquirer
-        .prompt([
-            {
-                type: 'list',
-                name: 'start',
-                message: 'What would you like to do?',
-                choices: initOptions,
-            }])
-        .then(answer => {        
-            switch(answer.start) {
-                case 'View All Departments':
-                    connect = new dbQuery('SELECT * FROM department');
-                    connect.getDepartments();
-                    break;
-                case 'View All Roles':
-                    connect = new dbQuery('SELECT * FROM role');
-                    connect.getRoles();
-                    break;
-                case 'View All Employees':
-                    connect = new dbQuery('SELECT * FROM employee');
-                    connect.getEmployees();
-                    break;
-                case 'Add a Department':
-                    addDepartment();
-                    break;
-                case 'Add a Role':
-                    // function
-                    break;
-                case 'Add An Employee':
-                    // connect = new dbQuery('INSERT INTO employee (id, first_name, last_name, role_id, manager_id) VALUES ('NULL', ${this.fname}, ${this.lname}, ${this.role}, ${this.manager});');
-                    // connect.addEmployee();
-                    // function
-                    break;
-                case 'Update An Employee Role':
-                    // connect = new dbQuery('UPDATE employee SET address = 'Canyon 123' WHERE address = 'Valley 345'');
-                    // connect.updateEmployee();
-                    // function 
-                    break;            
+        let connect;
+
+        switch(answer.start) {
+            case 'View All Departments':
+                connect = new dbQuery('SELECT * FROM department');
+                connect.getDepartments();
+                setTimeout(() => {init()},1000);
+                break;
+            case 'View All Roles':
+                connect = new dbQuery('SELECT * FROM role');
+                connect.getRoles();
+                setTimeout(() => {init()},1000);
+                break;
+            case 'View All Employees':
+                connect = new dbQuery('SELECT * FROM employee');
+                connect.getEmployees();
+                break;
+            case 'Add a Department':
+                addDepartment();
+                break;
+            case 'Add a Role':
+                addRole();
+                break;
+            case 'Add An Employee':
+                // connect = new dbQuery('INSERT INTO employee (id, first_name, last_name, role_id, manager_id) VALUES ('NULL', ${this.fname}, ${this.lname}, ${this.role}, ${this.manager});');
+                // connect.addEmployee();
+                // function
+                break;
+            case 'Update An Employee Role':
+                // connect = new dbQuery('UPDATE employee SET address = 'Canyon 123' WHERE address = 'Valley 345'');
+                // connect.updateEmployee();
+                // function 
+                break;
+            case 'Exit':
+                myDb.end();
+                break;         
         }
-    });  
+    });
 }
 
-const addDepartment = async () => {
+// const viewAllDept = () => {    
+//     connect = new dbQuery('SELECT * FROM department');
+//     connect.getDepartments();
+// }
+
+const addDepartment = () => {
     inquirer
         .prompt(addDepartmentQuestion)
         .then(answer => {
             // check if department name already exist
             connect = new modifyDb(`SELECT dept_name FROM department WHERE dept_name = '${answer.dept_name}'`, answer.dept_name);
             connect.addDept();
-            init();
+            setTimeout(() => {init()},1000);
         });   
 };
 
-const addRole = () => {};
+const addRole = () => {
+    inquirer
+        .prompt(addRoleQuestions)
+        .then(answers => {
+            let responses = [answers.role_name, answers.salary, answers.role_dept];
+            // check if department name already exist
+            connect = new modifyDb(`SELECT title FROM role WHERE title = '${answers.role_name}'`, responses);
+            connect.addRole();
+            setTimeout(() => {init()},1000);
+        });   
+};
 
 const addEmployee = () => {};
 
